c2bcff957bc6be012237b8e0e83578c5e46e76ea
Haruka Katagiri <harukatl0ve@gmail.com>
2020-04-19 16:19:14 +0900

    コミットコメント

--- diary/models.py
+++ diary/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
+app_name = 'diary'
 
 class Day(models.Model):
-    title = models.CharField('タイトル', max_length=100)
+    title = models.CharField('タイトル', max_length=200)
     text = models.TextField('本文')
-    date = models.DateField('日付', default=timezone.now)
+    date = models.DateTimeField('日付', default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- diary/views.py
+++ diary/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 from .forms import DayCreateForm
 from .models import Day
+from django.contrib.auth.mixins import LoginRequiredMixin # 追加
 
 # Create your views here.
 
@@ -10,19 +11,22 @@ class IndexView(generic.ListView):
     template_name = 'diary/day_list.html'
     model = Day
 
-class AddView(generic.CreateView):
+class AddView(LoginRequiredMixin, generic.CreateView): # 修正
+ # class AddView(generic.CreateView):
     template_name = 'diary/day_form.html'
     model = Day
     form_class = DayCreateForm
     success_url = reverse_lazy('diary:index')
 
-class UpdateView(generic.UpdateView):
+class UpdateView(LoginRequiredMixin, generic.UpdateView):  # 修正
+ # class UpdateView(generic.UpdateView):
     template_name = 'diary/day_form.html'
     model = Day
     form_class = DayCreateForm
     success_url = reverse_lazy('diary:index')
 
-class DeleteView(generic.DeleteView):
+class DeleteView(LoginRequiredMixin, generic.DeleteView):  # 修正
+ # class DeleteView(generic.DeleteView):
     template_name = 'diary/day_confirm_delete.html'
     model = Day
     success_url = reverse_lazy('diary:index')
